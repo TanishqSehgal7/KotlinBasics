4d939ceeac9050fe57cdb2a602f5a92a0162b90b
TanishqSehgal7 <43927740+TanishqSehgal7@users.noreply.github.com>
2020-07-22 19:46:16 +0530

    Update User.kt

--- User.kt
+++ User.kt
@@ -1,42 +1,70 @@
+import java.util.ArrayList
+
 fun main(){
 
     // creating the object of this class
-    val account:Account=Account()
-    account.newAccount(77777,"Tanishq Sehgal",20000f)
+    val account:Account=Account(77777,"Tanishq Sehgal",20000f)
+    val list= ArrayList<Int>(5)
     account.deposit(50000f)
     account.withDraw(20000f)
     account.withDraw(10000f)
-    println(account.Amount)
-    println("Account number:${account.account_no} , \n Account Holder Name:${account.Name}, Account Balance:${account.Amount}")
+    println(account.amount)
+    println("Account number:${account.amount} , \n Account Holder Name:${account.name}, Account Balance:${account.amount}")
 
 }
 
+// this keyword -  points to the current receiver ie, it always points to the current class reference
+/* constructors in a kotlin class-  a function that has the same name as that of the class, and is used to construct or initialize the class
+ types of constructors -  1. Primary constructors ; 2. Secondary  constructors
+ We can only have one Primary constructors while there can be multiple constructors*/
 
-class Account {
-    var account_no:Int=0
-    var Name:String?=null
-    var Amount:Float=0f
+//primary constructors can also be written using the init blocks
 
+/*
+Secondary constructors - created using the keyword - constructor
+ */
 
-    fun newAccount(acc:Int,name:String,amount:Float){
-        account_no=acc
-        Name=name
-        Amount=amount
+class Account  {
+
+    var accNo:Int=1
+    var name:String=""
+    var amount:Float=0f
+
+    init {
+        this.accNo=accNo
+        this.name=name
+        this.amount=amount
+    }
+
+    constructor(accNo:Int){
+       this.accNo=accNo
+        name="Default"
+        amount=0f
+    }
+    constructor(accNo:Int,name: String){
+        this.accNo=accNo
+        this.name="Default"
+        this.amount=0f
+    }
+    constructor(accNo:Int,name: String,amount:Float){
+        this.accNo=accNo
+        this.name=name
+        this.amount=amount
     }
 
     fun deposit(money:Float){
-        Amount+=money
-        println(Amount)
+        amount+=money
+        println(amount)
     }
 
     fun withDraw(money:Float){
 
-        if(Amount <money){
+        if(amount <money){
             println("Not enough money to withdraw.")
         }
         else{
-            Amount-=money
-            println(Amount)
+            amount-=money
+            println(amount)
         }
     }
 }
